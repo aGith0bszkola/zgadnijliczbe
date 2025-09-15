723ac3e3f10d942017bfd396f2a8be1a42b66b0e
aGith0bszkola <githubszkola@gmail.com>
2025-09-15 14:31:13 +0200

    typy proste

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("hello world");
+        // typy proste
+        //zmienne nie obikety - pisane małymiu listerami
+        //typy całkowite
+        byte liczba1 = 120;
+        short liczba2 = 300;
+        int liczba3 = 1234321;
+        long lczba4 = 1235435;
+        System.out.println("liczba całkowita"+liczba3);
+        //typy zmienne
+        float liczba5  = 12.456f;
+        double liczba6 = 23.456789;
+        //typ logiczny
+        boolean czyfalse = true;
+        //typ znakowy
+        char znak = 'a';
+
+
+
     }
 }
